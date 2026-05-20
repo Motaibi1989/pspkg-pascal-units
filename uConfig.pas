3ee64f5345e7060b75d8bb669b42892bb2b0c312
Mohammed Alotaibi <Motaibi1989@gmail.com>
2026-05-20 15:52:40 +0300

    Add configuration unit

---
+++ uConfig.pas
@@ -0,0 +1,14 @@
+unit uConfig;
+
+{$mode objfpc}{$H+}
+
+interface
+
+const
+  APP_NAME    = 'pspkg Pascal System Toolkit';
+  APP_VERSION = '2.0.0';
+  APP_AUTHOR  = 'Mohammed Alotaibi';
+
+implementation
+
+end.
